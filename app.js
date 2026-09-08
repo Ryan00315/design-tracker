a8a9307f75f25b35720845e6303b413894aa9efb
Ryan00315 <97594075+Ryan00315@users.noreply.github.com>
2026-09-08 15:39:31 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2346,7 +2346,16 @@ function renderAdHocEvents() {
     // 將「共計」的 td 也設定為 text-align: right
     tr.innerHTML = `
       <td style="white-space: nowrap; width: 1%;"><strong>${evt.title}</strong></td>
-      <td style="word-break: break-all; width: 100%; min-width: 200px;">${evt.reason}</td>
+      <td style="white-space: nowrap; width: 1%;"><strong>${evt.title}</strong></td>
+      <td style="word-break: break-all; width: 100%; min-width: 200px;">
+          <!-- 幫它穿上 Quill 的樣式外衣，並消除多餘的邊距 -->
+          <div class="ql-snow" style="border: none;">
+              <div class="ql-editor" style="padding: 0; min-height: auto; font-size: inherit;">
+                  ${evt.reason}
+              </div>
+          </div>
+      </td>
+      <td style="white-space: nowrap; text-align: right; color: #475569;">${sDateStr}</td>
       <td style="white-space: nowrap; text-align: right; color: #475569;">${sDateStr}</td>
       <td style="white-space: nowrap; text-align: right; color: #475569;">${cDateStr}</td>
       <td style="white-space: nowrap; text-align: right; padding-right: 8px;">${durationText}</td>
